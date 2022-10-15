062d4d83178176f04b0765496efa923c5ad83554
Jordi2233 <337236@uwr.edu.pl>
2022-10-15 08:51:42 +0200

    Code clearing

--- test.py
+++ test.py
@@ -2,15 +2,14 @@ import numpy as np
 from termcolor import colored
 import os as s
 
-s.system("clear")
+s.system("clearrr")
 
 colors = ['grey', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
-a = np.arange(5 * 5).reshape(5, 5)
+arr = np.arange(5 * 5).reshape(5, 5)
 
-for x in colors:
-    if (x == "grey"):
-        print(colored(x, x, "on_white"),":\n", colored(a, x, "on_white"), "\n")
+for color in colors:
+    if (color == "grey"):
+        print(colored(color, color, "on_white"),":\n", colored(arr, color, "on_white"), "\n")
     else:
-        print(colored(x, x),":\n", colored(a, x), "\n")
-print(a.ndim)
+        print(colored(color, color),":\n", colored(arr, color), "\n")
