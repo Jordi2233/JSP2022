f6552e7158c0417068761611fd82e1bcc5b493ca
Jordi2233 <337236@uwr.edu.pl>
2022-10-19 12:33:47 +0200

    Complete List1

--- Lista1/Zad10.py
+++ Lista1/Zad10.py
@@ -1,3 +1,29 @@
 import os as s
+from math import *
+from cmath import *
+
 
 s.system("clear")
+
+z = complex(0, 1)
+
+sin_z = sin(z)
+cos_z = cos(z)
+
+L_r = "Liczba rzeczysista z "
+L_im = "Liczba zespolona z "
+
+print("z =", z)
+
+print()
+
+print(L_r + "sin(z):" ,sin_z.real, "\n", L_im + "sin(z): ", sin_z.imag)
+
+print()
+
+print(L_r + "cos(z):", cos_z.real, "\n", L_im + "cos(z):", cos_z.imag)
+
+
+
+test = sin_z**2 + cos_z**2
+print("sin^2(z) + cos^2(z) =", test)
