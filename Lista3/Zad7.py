6fa88794c97b8c9b1aad2c0726e69f254c6dfdb3
Jordi2233 <337236@uwr.edu.pl>
2022-11-06 09:12:28 +0100

    Little changes and Lista5/ dir created

--- Lista3/Zad7.py
+++ Lista3/Zad7.py
@@ -5,16 +5,14 @@ n = int(input("Enter the last element of the Fibonacci sequence: "))
 
 fib_arr = [0, 1, 1]
 
-
-f_len = len(fib_arr)
-
-for x in range(f_len, n):
+for x in range(len(fib_arr), n):
     fib_arr.append(fib_arr[x-1] + fib_arr[x-2])
 
+print(f"Last element of your sequence: {fib_arr[n-1]}")
 
-for x in range(len(fib_arr)):
-    system("sleep 0.2")
-    print(str(x + 1) + ") ", fib_arr[x])
+# for x in range(len(fib_arr)):
+#     system("sleep 0.2")
+#     print(  f"{x + 1} ) \t{fib_arr[x]}" )
 
 
 
