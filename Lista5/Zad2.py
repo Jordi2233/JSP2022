781710812c12794f8c1a176bfab2b1442a29c471
Jordi2233 <User123.Name456@protonmail.com>
2022-11-23 11:31:21 +0100

    test

--- Lista5/Zad2.py
+++ Lista5/Zad2.py
@@ -1,8 +1,12 @@
 import os
+from num2words import num2words
 
 
 def main():
     os.system('clear')
+    num: int = input("Enter the number: ")
+
+    print(num2words(num))
 
 if __name__ == '__main__':
     main()
