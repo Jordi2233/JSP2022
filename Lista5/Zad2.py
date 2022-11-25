9da55a5c6704793e6f61840ba0578084bda91d82
Jordi2233 <User123.Name456@protonmail.com>
2022-11-25 09:30:27 +0100

    from 0 to 100 ened Zad2

--- Lista5/Zad2.py
+++ Lista5/Zad2.py
@@ -1,5 +1,8 @@
 import os
 
+def decimal(num: int) -> int:
+    res = (num - (num % 10))
+    return res
 
 def from_num_to_word(user_number: int) -> str:
 
@@ -13,17 +16,35 @@ def from_num_to_word(user_number: int) -> str:
         7: "siedem",
         8: "osiem",
         9: "dziewięć",
-        10: "dziesięć"
+        10: "dziesięć",
+        11: "jedenaście",
+        12: "dwanaście",
+        13: "trzynaście",
+        14: "czternaście",
+        15: "piętnaście",
+        16: "szesnaście",
+        17: "siedemnaście",
+        18: "osiemnaście",
+        19: "dziewiętnaście",
+        20: "dwadzieścia",
+        30: "trzydzieści",
+        40: "czterdzieści",
+        100: "sto",
+        1000: "tysiąc"
     }
 
-
-
-    if user_number % 10 == 0:
-        res = "Podzielna przez 10"
+    if user_number < 100 and user_number not in numbers:
+        if user_number % 10 != 0 and user_number >= 50:
+            key = decimal(user_number) / 10
+            unit = user_number % 10
+            res = f'{numbers[key]}dziesiąt {numbers[unit]}'
+        else:
+            key = user_number % 10
+            res = f'{numbers[decimal(user_number)]} {numbers[key]}'
     else:
         res: str = numbers[user_number]
-
     return res
+
 
 def main():
     os.system('clear')
