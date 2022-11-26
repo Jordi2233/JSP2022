690403e1f393918adb40bcaaa4fd37ef2db180ce
Jordi2233 <User123.Name456@protonmail.com>
2022-11-26 10:50:38 +0100

    Finished Zad4

--- Lista5/Zad4.py
+++ Lista5/Zad4.py
@@ -1,15 +1,41 @@
+#!/usr/bin/env python3
 import os
 
-def main():
-<<<<<<< HEAD
-   os.system('clear')
-   print("test")
 
-if __name__ == '__main__':
-  main()
-=======
+class cipher:
+
+    def encrypting(writing: str, key: dict) -> str:
+        encrypted_writing = ""
+        for letter in writing:
+            if letter in key:
+                letter = key[letter]
+                encrypted_writing += letter
+            else:
+                encrypted_writing += letter
+        return encrypted_writing
+
+    def decrypting(writing: str, key: dict):
+        encrypted_writing = ""
+        for letter in writing:
+            if letter in key.values():
+                letter = [i for i in key if key[i]==letter]
+                letter = ''.join(letter)
+                encrypted_writing += letter
+            else:
+                encrypted_writing += letter
+        return encrypted_writing
+
+
+def main():
     os.system('clear')
+    user_str: str = input("Enter the phase: ")
+    key = {"a": "y", "e": "i", "i": "o", "o": "a", "y": "e"}
+    # user_str: str = "to jest moj tekst"
+    encrypted_text = cipher.encrypting(user_str, key)
+    decrypted_text = cipher.decrypting(encrypted_text, key)
+    print(encrypted_text)
+    print(decrypted_text)
+
 
 if __name__ == '__main__':
     main()
->>>>>>> 79958ba085f51ba7c9d339f26761d1a23fb58cd6
