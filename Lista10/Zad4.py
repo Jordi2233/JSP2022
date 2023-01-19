22387d13b1e3e028f861cc76b3639de8fc66b55a
Jordi2233 <User123.Name456@protonmail.com>
2023-01-19 12:06:15 +0100

    List10 completed

--- Lista10/Zad4.py
+++ Lista10/Zad4.py
@@ -2,18 +2,47 @@
 import os
 from bs4 import BeautifulSoup
 import requests
+from dotenv import dotenv_values
+from datetime import date, timedelta
 
 
-def get_html(url: str, path: str) -> str:
-    response = requests.get(url)
-    open(path, 'w').write(response.text)
+class Currency:
+    url = 'https://www.nbp.pl/kursy/xml/a012z230118.xml'
+
+    def __init__(self, path: str):
+        self.path = path
+        self.file = BeautifulSoup(open(path), 'xml')
+        Currency.get_html(self, self.url)
+
+    def get_html(self, url: str):
+        if not os.path.isfile(self.path) or self.file.find('data_publikacji').text != (date.today() - timedelta(days=1)).strftime('%Y-%m-%d'):
+            response = requests.get(url)
+            open(self.path, 'w').write(response.text)
+
+    def get_currency(self, code: str):
+        code_parent = self.file.find('kod_waluty', text=code).parent
+        exchange_rate = float(code_parent.find('kurs_sredni').text.replace(',', '.'))
+        return exchange_rate
+
+    def calculate_pln(self, code: str, amount: float):
+        exchange_rate = Currency.get_currency(self, code)
+        return f'{amount} {code} = {amount * exchange_rate} PLN'
+
+    def calculate_curr_curr(self, code_1: str, code_2: str, amount: float):
+        currency_1 = Currency.get_currency(self, code_1)
+        currency_2 = Currency.get_currency(self, code_2)
+        return f'{amount} {code_1} = {round((amount * currency_1 / currency_2), 3)} {code_2}'
+
+
 
 
 def main():
     os.system('clear')
-    url = 'https://www.nbp.pl/kursy/xml/a012z230118.xml'
-    path = 'Lista10/kursy.xml'
-    get_html(url, path)
+    path = dotenv_values()['PATH']
+    currency = Currency(path)
+    print(currency.calculate_pln('THB', 100))
+    print(currency.calculate_curr_curr('USD', 'THB', 100))
+
 
 if __name__ == '__main__':
     main()
